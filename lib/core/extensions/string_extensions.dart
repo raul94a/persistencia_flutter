08841c3e94cc8fa69288e34922132f6757dba16b
raul.albin <raul200794@gmail.com>
2024-02-06 19:43:40 +0100

    secure capitalize first

--- lib/core/extensions/string_extensions.dart
+++ lib/core/extensions/string_extensions.dart
@@ -1,6 +1,9 @@
 extension CapitalizeFirst on String {
   String capitalizeFirst() {
     assert(isNotEmpty, 'String cannot be empty');
+    if (length == 1) {
+      return toUpperCase();
+    }
     return substring(0, 1).toUpperCase() + substring(1);
   }
 }
